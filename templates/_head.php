48868b095bde991f72492fcfba8ccec35537e3a5
hubertkoy <hubkoydev@gmail.com>
2021-07-02 16:36:41 +0100

    Set title

--- templates/_head.php
+++ templates/_head.php
@@ -7,7 +7,7 @@ $loggedIn = Session::getInstance()->isAuthorized();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Title</title>
+    <title>File Manager</title>
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="../assets/css/style.css" rel="stylesheet">
